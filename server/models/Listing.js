22f5234dd1d2d8f166f6399241db6b18860f027d
Isbah Kassam <isbahashif06@gmail.com>
2026-03-17 10:38:16 +0300

    created a schema that will handle all product details

--- server/models/Listing.js
+++ server/models/Listing.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const listingSchema = new mongoose.Schema({
+  // this is for the prduct information
   name: { type: String, required: true },
   weight: { type: Number, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
-  image: { type: String }, // Stores the URL string
+  
   category: { type: String, required: true }, 
   status: { type: String, default: "Active" }
 }, { timestamps: true });
-
+// "Listing" will be the collection name in MongoDB
 module.exports = mongoose.model('Listing', listingSchema);
